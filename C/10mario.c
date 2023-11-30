cd2d6b9a0994c3f837ca5ebc4d16375a404f76be
Victor Alano <138694685+alanovictor@users.noreply.github.com>
2023-11-30 20:12:33 -0300

    Initial commit

---
+++ C/10mario.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  for (int i = 0; i<4; i++)
+  {
+    printf("?");
+  }
+  printf("\n");
+  for (int i = 0; i<4; i++)
+  {
+    printf("#\n");
+  }
+    int n = get_int("Size: ");
+    while (n < 1) 
+    {
+      printf("Invalid information, type again!");
+      n = get_int("Size: ");
+    }
+    for (int i = 0; i < n; i++)
+  {
+      for(int j = 0; j < n; j++)
+      {
+        printf("#");
+      }
+      printf("\n");
+  }
+  
+  n = 0;
+  n = get_int("Size: ");
+    do
+    {
+      n = get_int("Size: ");
+    }
+    while(n < 1);
+
+    for (int i = 0; i < n; i++)
+  {
+      for(int j = 0; j < n; j++)
+      {
+        printf("#");
+      }
+      printf("\n");
+  }
+  
+}
