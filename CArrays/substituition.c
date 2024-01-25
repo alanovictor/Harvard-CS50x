f6c8ea31194154c16e1b8c23794cab6e0181970c
Victor Alano <138694685+alanovictor@users.noreply.github.com>
2024-01-25 20:15:25 -0300

    subs

---
+++ CArrays/substituition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h> 
+
+int main(int argc, string argv[]){
+
+if (argc != 2){
+printf("Usage: substituition key");
+return 1;
+}
+
+printf("%s", argv[1]);
+
+}
