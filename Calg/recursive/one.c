e6485e82b81fa7ca8aebcc49ec76fcd269462b17
Victor Alano <138694685+alanovictor@users.noreply.github.com>
2024-02-20 19:45:33 -0300

    recursive

--- Calg/recursive/one.c
+++ Calg/recursive/one.c
@@ -8,15 +8,22 @@ int main(void){
   draw(height);
 }
 
-void draw(int n){ // Print pyramid of height n -1
+void draw(int n){ 
+  // If nothing to draw
+  if (n <= 0){
+    return;
+  }
+  // Print pyramid of height n -1
   draw(n - 1);
 
   // Print one more row
-  for (int i = 0; i < n; i++{
+  for (int i = 0; i < n; i++){
     printf("#");
   }
   printf("\n");
   }
-}
+
+
+  // gcc -o one .\one.c .\cs50.c
 
 
