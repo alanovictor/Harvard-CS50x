03cd8aa11f8565e9587b05687fff086a144e6d50
Victor Alano <138694685+alanovictor@users.noreply.github.com>
2024-04-26 13:24:21 -0300

    lecture ds

--- 5-CDataStructures/Lecture/list.c
+++ 5-CDataStructures/Lecture/list.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h> // da acesso ao malloc
 
 int main(void){
-  int list[3];
+  // int list[3]; agora vamos usar o malloc
+  int *list = malloc(3 * sizeof(int));
+  if (list == NULL){
+    return 1;
+  }
 
   list[0] = 1;
   list[1] = 2;
   list[2] = 3;
+
+  for(int i = 0; i < 3; i++){
+    printf("%i\n", list[i]);
+  }
 }
+
+//gcc -o list .\list.c .\cs50.c
